9b8dd0528bbf7641f1492e9a8e5693a6fe7659b3
Toxica20002 <dinhquoclam02@gmail.com>
2024-05-09 00:16:53 +0700

    fix client and server

--- Lab5_ITITIU20002_LamQuocDinh_NetCentric/client/client.go
+++ Lab5_ITITIU20002_LamQuocDinh_NetCentric/client/client.go
@@ -8,11 +8,29 @@ import (
 	"strings"
 )
 
+type MessageQueue []string
+
+func (mq *MessageQueue) Push(message string) {
+	*mq = append(*mq, message)
+}
+
+func (mq *MessageQueue) Pop() (string, bool) {
+	if len(*mq) == 0 {
+		return "", false
+	}
+	message := (*mq)[0]
+	*mq = (*mq)[1:]
+	return message, true
+}
+
+var messageQueue MessageQueue
+
 func main() {
 	if len(os.Args) != 3 {
 		fmt.Fprintf(os.Stderr, "Usage: %s host:port username", os.Args[0])
 		os.Exit(1)
 	}
+
 	service := os.Args[1]
 	username := os.Args[2]
 	udpAddr, err := net.ResolveUDPAddr("udp", service)
@@ -29,21 +47,35 @@ func main() {
 	// Read messages from stdin and send them to the server
 	reader := bufio.NewReader(os.Stdin)
 	for {
-		fmt.Print("Enter recipient (@username or @all): ")
+		fmt.Print("Enter recipient (@username or @all) or Check message(@message): ")
 		recipient, _ := reader.ReadString('\n')
 		recipient = strings.TrimSpace(recipient)
 
-		fmt.Print("Enter message: ")
-		message, _ := reader.ReadString('\n')
-		message = strings.TrimSpace(message)
+		if recipient != "@message" {
+			fmt.Print("Enter message: ")
+			message, _ := reader.ReadString('\n')
+			message = strings.TrimSpace(message)
 
-		fullMessage := recipient + " " + message
-		_, err = conn.Write([]byte(fullMessage))
-		checkError(err)
+			fullMessage := recipient + " " + message + " " + username
+			_, err = conn.Write([]byte(fullMessage))
+			checkError(err)
+		} else {
 
-		if message == "logout" {
-			break
+			//Print messages from the server
+			fmt.Println("\n***Messages from the server:***")
+
+			for {
+				message, ok := messageQueue.Pop()
+				if !ok {
+					break
+				}
+				fmt.Println(message)
+			}
+
+			fmt.Println("***End of messages from the server***\n")
+
 		}
+
 	}
 
 	os.Exit(0)
@@ -54,7 +86,8 @@ func readMessages(conn *net.UDPConn) {
 	for {
 		n, err := conn.Read(buf[0:])
 		checkError(err)
-		fmt.Println(string(buf[0:n]))
+		message := string(buf[:n])
+		messageQueue.Push(message)
 	}
 }
 
--- Lab5_ITITIU20002_LamQuocDinh_NetCentric/server/server.go
+++ Lab5_ITITIU20002_LamQuocDinh_NetCentric/server/server.go
@@ -5,18 +5,14 @@ import (
 	"net"
 	"os"
 	"strings"
-	"sync"
-	"time"
 )
 
 type ClientInfo struct {
-	IP            string
-	Name          string
-	LastHeartbeat time.Time
+	IP   string
+	Name string
 }
 
 var clients = make(map[string]ClientInfo)
-var mutex = &sync.Mutex{}
 
 func main() {
 	service := ":1200"
@@ -24,7 +20,6 @@ func main() {
 	checkError(err)
 	conn, err := net.ListenUDP("udp", udpAddr)
 	checkError(err)
-	go checkHeartbeats()
 	for {
 		handleClient(conn)
 	}
@@ -38,39 +33,30 @@ func handleClient(conn *net.UDPConn) {
 	}
 	message := string(buf[:n])
 
-	mutex.Lock()
 	//fmt.Println("Received: ", message)
 	if strings.HasPrefix(message, "@") {
-		parts := strings.SplitN(message, " ", 2)
+		parts := strings.SplitN(message, " ", 3)
 		command := parts[0][1:] // remove the '@'
-		fmt.Println("message: ", message)
-		fmt.Println("command: ", command)
 		if len(parts) > 1 {
 			msg := parts[1]
+			sender := parts[2]
 			if command == "all" {
-				// fmt.Println("Broadcasting message: ", msg)
-				// fmt.Println("Clients: ", clients)
-				for key, client := range clients {
-					fmt.Println("Client: ", client)
-					clientAddr, _ := net.ResolveUDPAddr("udp", key) // assuming clients are listening on port 1200
+				for addr, client := range clients {
+					if client.Name == sender {
+						continue
+					}
+					msg = "(Public) " + sender + ": " + msg
+					clientAddr, _ := net.ResolveUDPAddr("udp", addr) // assuming clients are listening on port 1200
 					conn.WriteToUDP([]byte(msg), clientAddr)
-					fmt.Println("ClientAddr: ", clientAddr)
-					fmt.Println("Key: ", key)
 				}
 			} else {
-				// if client, ok := clients[command]; ok {
-				// 	clientAddr, _ := net.ResolveUDPAddr("udp", client.IP+":1200") // assuming clients are listening on port 1200
-				// 	conn.WriteToUDP([]byte(msg), clientAddr)
-				// } else {
-				// 	errMsg := "The user " + command + " does not exist."
-				// 	conn.WriteToUDP([]byte(errMsg), addr)
-				// }
+				msg = "(Private) " + sender + ": " + msg
 				name := command
 				name = strings.TrimSpace(name)
 				flag := false
-				for key, client := range clients {
+				for addr, client := range clients {
 					if client.Name == name {
-						clientAddr, _ := net.ResolveUDPAddr("udp", key) // assuming clients are listening on port 1200
+						clientAddr, _ := net.ResolveUDPAddr("udp", addr) // assuming clients are listening on port 1200
 						conn.WriteToUDP([]byte(msg), clientAddr)
 						flag = true
 					}
@@ -82,27 +68,12 @@ func handleClient(conn *net.UDPConn) {
 			}
 		}
 	} else if client, ok := clients[addr.String()]; ok {
-		client.LastHeartbeat = time.Now()
 		clients[addr.String()] = client
 	} else {
 		// fmt.Println("New client registered: " + message + " from " + addr.String())
 		message = strings.TrimSpace(message)
-		clients[addr.String()] = ClientInfo{IP: addr.IP.String(), Name: message, LastHeartbeat: time.Now()}
+		clients[addr.String()] = ClientInfo{IP: addr.IP.String(), Name: message}
 		fmt.Println("New client registered: " + message + " from " + addr.String())
-	}
-	mutex.Unlock()
-}
-
-func checkHeartbeats() {
-	for {
-		time.Sleep(5 * time.Second)
-		mutex.Lock()
-		for addr, client := range clients {
-			if time.Since(client.LastHeartbeat) > 10*time.Second {
-				delete(clients, addr)
-			}
-		}
-		mutex.Unlock()
 	}
 }
 
